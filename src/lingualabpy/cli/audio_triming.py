78c4f18f70f03a6917d4cec1723b99a323714439
Christophe Bedetti <christophe.bedetti@umontreal.ca>
2024-07-04 16:32:21 -0400

    add remove_overlap option

--- src/lingualabpy/cli/audio_triming.py
+++ src/lingualabpy/cli/audio_triming.py
@@ -3,7 +3,7 @@ import click
 from lingualabpy import default_config, read_audio, read_textgrid
 from lingualabpy.audio.triming import extract_audio
 from lingualabpy.text.textgrid import extract_intervals
-from lingualabpy.tools.interval import intervals_masking
+from lingualabpy.tools.interval import intervals_masking, interval_to_list
 
 
 @click.command()
@@ -17,10 +17,13 @@ from lingualabpy.tools.interval import intervals_masking
     default=default_config["clinician_label"],
     show_default=True,
 )
+@click.option("--remove_overlap", is_flag=True, show_default=True)
 @click.argument("textgrid", nargs=1, type=click.Path(exists=True))
 @click.argument("audiofile", nargs=1, type=click.Path(exists=True))
 @click.argument("output", nargs=1)
-def main(participant_label, clinician_label, textgrid, audiofile, output):
+def main(
+    participant_label, clinician_label, remove_overlap, textgrid, audiofile, output
+):
     """Doc"""
     grid = read_textgrid(textgrid)
 
@@ -31,12 +34,15 @@ def main(participant_label, clinician_label, textgrid, audiofile, output):
     except Exception as e:
         raise Exception(f"Failed to extract intervals for {textgrid}", repr(e))
 
-    participant_intervals_clean = intervals_masking(
-        participant_intervals, clinician_intervals
-    )
+    if remove_overlap:
+        participant_intervals = intervals_masking(
+            participant_intervals, clinician_intervals
+        )
+    else:
+        participant_intervals = map(interval_to_list, participant_intervals)
 
     audio = read_audio(audiofile)
 
-    audio_clean = extract_audio(audio, participant_intervals_clean)
+    audio_clean = extract_audio(audio, participant_intervals)
 
-    audio_clean.export(output)
+    audio_clean.export(output, format="wav")
