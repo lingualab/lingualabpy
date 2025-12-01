d4dcbc2e0dda05bbca9734d778e6c76cf4f61989
Christophe Bedetti <christophe.bedetti@umontreal.ca>
2025-12-01 11:52:34 -0500

    update to hatch and gitignore grom github

--- src/lingualabpy/__init__.py
+++ src/lingualabpy/__init__.py
@@ -2,9 +2,14 @@
 #   Licensed under the MIT License. See LICENSE in project root for information.
 #   -------------------------------------------------------------
 """lingualabpy"""
+
 from __future__ import annotations
 
-__version__ = "0.0.6"
+try:
+    from lingualabpy._version import __version__
+except ImportError:
+    pass
+
 
 default_config = {
     "participant_col": "participant_id",
